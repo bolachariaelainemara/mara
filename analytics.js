0b875da4680c046ee4868f54e000cff7825a5448
bolachariaelainemara <bolachariaelainemara@gmail.com>
2026-09-24 10:10:40 -0300

    analytics.js

---
+++ analytics.js
@@ -0,0 +1,14 @@
+// Substitua o 'G-SEU-CODIGO-AQUI' pelo seu ID real do Google Analytics
+var gaId = 'G-2CBFDQ8B53';
+
+// Carrega o script oficial do Google Analytics
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=' + gaId;
+document.head.appendChild(script);
+
+// Inicializa a navegação e eventos
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', gaId);
